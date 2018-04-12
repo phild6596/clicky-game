00649301c9f547c7282d2f637dbfae30cab2465c
phild6596 <phild6596@gmail.com>
2018-04-11 19:14:52 -0700

    testing minor routes to make sure react compiled succesfully. it works

---
+++ src/components/gameInstructions/GameInstructions.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./GameInstructions.css";
+
+const GameInstructions = props => (
+    <div className="col-md-12">
+        <div className="gameInstructions">
+            <h1>Game Instructions:</h1>
+            <h2>Click on any image, but never click on the same one twice, or you will lose!</h2>
+        </div>
+    </div>
+);
+
+
+export default GameInstructions;
