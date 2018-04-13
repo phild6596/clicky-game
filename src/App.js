ced008cad8ada33d33387b7e159a9dc34ea8c21c
phild6596 <phild6596@gmail.com>
2018-04-13 14:37:49 -0700

    added fucntionality to game, although had to rewrite render() in app.js, wouldnt randomize cards unless this.state.friends.map had an object.

--- src/App.js
+++ src/App.js
@@ -9,22 +9,94 @@ import './App.css';
 
 class App extends Component {
   state = {
-    friends
+    score: 0,
+    highScore: 0,
+    friends: friends
   };
 
+  randomRender = id => {
+    this.state.friends.forEach(image => {
+      if (image.id === id) {
+        if (image.clicked) {
+          alert("You lose, that card was already selected");
+          this.setState({});
+          this.resetGame();
+          return false;
+        } else {
+          this.updateScore();
+          image.clicked = true;
+        }
+        if (this.state.score >= this.state.highScore) {
+          this.newHighScore();
+        }
+      }
+    });
+  };
+
+  randomOrganize = array => {
+    let copy = [],
+      n = array.length,
+      i;
+    while (n) {
+      i = Math.floor(Math.random() * array.length);
+      if (i in array) {
+        copy.push(array[i]);
+        delete array[i];
+        n--;
+      }
+    }
+    this.setState({ friends: copy });
+  };
+
+  updateScore = () => {
+    this.setState(
+      newState => ({
+        score: newState.score + 1
+      }),
+      () => this.winning()
+    );
+  };
+
+  newHighScore = () => {
+    this.setState(newState => ({
+      highScore: newState.score
+    }));
+  };
+
+  winning = () => {
+    if (this.state.score === this.state.friends.length) {
+      alert("YOU WIN!! congratulations!");
+      this.setState({});
+      this.resetGame();
+    } else {
+      setTimeout(() => {
+        this.randomOrganize(this.state.friends);
+      }, 500);
+    }
+  };
+
+  resetGame = () => {
+    this.state.friends.forEach(image => {
+      image.cliked = false;
+    });
+    this.setState({ score: 0 });
+  };
 
   render() {
     return (
       <Wrapper>
         <Nav score={this.state.score} highScore={this.state.highScore} />
-          <GameInstructions />
-        {this.state.friends.map(friend => (
-           <FriendCard
-          key={friend.id}
-          name={friend.name}
-          image={friend.image}
+        <GameInstructions />
+        {this.state.friends.map(friend => {
+          return <FriendCard
+          {...friend}
+            key={friend.id}
+            //name={friend.name}
+            //image={friend.image}
+            randomRender={this.randomRender}
+            randomOrganize={() => this.randomOrganize(this.state.friends)}
           />
-        ))}
+        })}
       </Wrapper>
     );
   }
